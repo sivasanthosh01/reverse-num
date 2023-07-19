7ab25f1442a9633ed74687a71837a3c8147adf25
santhosh <killersanthosh0032@gmail.com>
2023-07-19 20:24:36 +0530

    reverse-num

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let a=prompt("enter the reverse number");
+let c=a;
+let result=0;
+var b;
+document.write("enter number:&nbsp;&nbsp;"+a+"<br>");
+while(a>0)
+{
+	b=a%10;
+	result=result*10+b;
+	a=parseInt(a/10);
+	console.log(a);
+
+}
+document.write("reversed number:&nbsp;&nbsp;"+result);
+
+	
